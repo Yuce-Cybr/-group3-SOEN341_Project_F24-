32e4fca4c4a89ffc79322e4787bd359ae674922f
david-makary <davmakary@gmail.com>
2024-09-28 23:51:14 -0400

    returning access token

--- API/server.js
+++ API/server.js
@@ -4,19 +4,28 @@ import createUser from './database/firbase.js';
 const app = express()
 const port = 3000
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 app.get('/', (req, res) => {
   res.send('hello world!')
 })
+
+
 
 app.post('/user', async (req, res) => {
     
     const { email, password } = req.body;
     try {
         const user = await createUser(email, password)
-        res.status(201).send(`User registered: ${user}`);
+        const response = {
+            "accessToken": user.stsTokenManager.accessToken
+        }
+        res.status(201).send(response);
     } catch (error) {
         console.error("Error registering user:", error.message);
     }
