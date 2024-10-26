6e4d9034b0ba5767a3677209d4efd01b6f302eb8
RedSquirrels <98779708+RedSquirrels@users.noreply.github.com>
2024-10-26 19:20:11 -0400

    clear file

--- Frontend/src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const UrlSupabase = 'https://bqfznzuhzegrhslwmmnz.supabase.co'
-const KeySupabase = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxZnpuenVoemVncmhzbHdtbW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyMTQzODMsImV4cCI6MjA0NDc5MDM4M30._vEbQDd3V6demk4i2zOm6oaDEXCCp2AZhNvFCtoF-gY'
-
-const supabase = createClient
-
-export default supabase
