4f887ff9f461a5e0ad509a7ae28a8d06544d8018
Sitherankan sinnappu <123910569+darkexcution@users.noreply.github.com>
2024-11-08 21:15:00 -0500

    Update App.test.js

--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,17 +1,16 @@
-// Import necessary dependencies
+import React from 'react';
 import { render } from '@testing-library/react';
-import { AuthProvider } from './AuthContext'; // Ensure this path matches your project structure
 import App from './App';
+import { AuthProvider } from './AuthContext';
 
-// Wrap App in the AuthProvider to mock context values
 test('renders learn react link', () => {
   render(
     <AuthProvider>
       <App />
     </AuthProvider>
   );
-  // Your existing test code here, e.g., checking for elements or text
 });
+
 
 
 
