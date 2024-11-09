7c87d3129f38d77f6f43c2a82d0e9bb52fa892db
Sitherankan sinnappu <123910569+darkexcution@users.noreply.github.com>
2024-11-08 20:59:37 -0500

    Update App.test.js

--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,14 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { AuthProvider } from './AuthContext'; // Import your AuthProvider
+import { AuthProvider } from './contexts/AuthContext'; // Adjust this import if needed
 
 test('renders learn react link', () => {
   render(
-    <AuthProvider>  {/* Wrap App with AuthProvider */}
+    <AuthProvider>
       <App />
     </AuthProvider>
   );
+  
+  // Add assertions based on what's expected in the App
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
 
