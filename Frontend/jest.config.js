0ba6c3123049cc8ae6f8f86638fc5b51eef91d20
ag-aryan <aryanaggarwal2401@gmail.com>
2024-11-28 19:11:48 -0500

    Update jest.config.js

--- Frontend/jest.config.js
+++ Frontend/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'], // Include .jsx extensions
   collectCoverage: true,
-  coverageDirectory: 'test-results', // Ensure results are saved here
+  coverageDirectory: './coverage',
   reporters: ['default', ['jest-junit', { outputDirectory: './test-results' }]],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest', // Use Babel to transform .js and .jsx files
+  },
 };
